b347054a4efe3425ee0bfa224baa1de103299520
Aswinjoseph07 <aswinjoseph9361@gmail.com>
2026-06-26 16:36:39 +0530

    first commit on new repo

---
+++ src/test/java/Smartscore/cgpa_gpa_calculator/CgpaGpaCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package Smartscore.cgpa_gpa_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgpaGpaCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
